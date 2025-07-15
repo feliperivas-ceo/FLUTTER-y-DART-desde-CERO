dee2710cdbba754b4c8958e00dee4aa9019cc837
morado123 <murillofabio894@gmail.com>
2025-07-15 14:44:52 -0500

    We start with a simple "print()" print

---
+++ dart_application_basics/bin/dart_application_basics.dart
@@ -0,0 +1,6 @@
+import 'package:dart_application_basics/dart_application_basics.dart' as dart_application_basics;
+
+//Iniciamos con una simple impresion "print()"
+void main(List<String> arguments) {
+  print('Hello world: ${dart_application_basics.calculate()}!');
+}
